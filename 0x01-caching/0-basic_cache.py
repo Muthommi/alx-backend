f4fc887f727ce5b5f517e42af1caaddace46e270
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-10-30 08:32:52 +0000

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,8 +7,10 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ Basic caching system with no size limit """
     def put(self, key, item):
+        """ Adds an item in the cache. """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ Gets an item by key. """
         return self.cache_data.get(key, None)
