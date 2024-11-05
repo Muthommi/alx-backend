df6ec87e3be7035b395350317ac9a18ac2f1a86c
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-05 09:07:11 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """Determines best match for supported languages."""
     return accept_languages.best_match(app.config['LANGUAGES'])
